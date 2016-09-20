e6f416fb2590cc0d20a0fdfd602ca394bc0ae78e
Kenneth Korcal <kakorcal@gmail.com>
2016-09-20 15:27:50 -0700

    word count

---
+++ src/prompts/13_word_count.js
@@ -0,0 +1,22 @@
+/*
+  DESCRIPTION:
+  have the function WordCount(str) take the str string parameter 
+  being passed and return the number of words the string contains 
+  (ie. "Never eat shredded wheat" would return 4). Words will be 
+  separated by single spaces. 
+*/
+
+function WordCount(str){
+  return str.match(/\S+/g).length;
+}
+
+module.exports = WordCount;
+
+/*
+  function WordCount(str) { 
+
+    // code goes here  
+    return str.split(' ').length; 
+           
+  }
+*/
---
+++ src/specs/13_word_count.spec.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const path = require('path');
+const WordCount = require(path.resolve('src/prompts', '13_word_count'));
+
+/*
+  Input:"Hello World"
+  Output:2
+
+  Input:"one 22 three"
+  Output:3
+*/
+
+describe('13_word_count', ()=>{
+  it('should be a function', ()=>{
+    assert.strictEqual(typeof WordCount, 'function');
+  });
+
+  it('should be a function', ()=>{
+    assert.strictEqual(WordCount('Hello World'), 2);
+    assert.strictEqual(WordCount('one 22 three'), 3);
+    assert.strictEqual(WordCount('letter sz!23 z'), 3);
+  });
+});
