077b3535f7f6393b8dd07a0baace1e5522b5dc36
Kenneth Korcal <kakorcal@gmail.com>
2016-09-21 13:24:52 -0700

    number addition

--- src/prompts/25_number_addition.js
+++ src/prompts/25_number_addition.js
@@ -1,15 +1,30 @@
 /*
   DESCRIPTION:
-
+  have the function NumberSearch(str) take the str parameter, search
+  for all the numbers in the string, add them together, then 
+  return that final number. For example: if str is "88Hello 3World!" the output
+  should be 91. You will have to differentiate between single digit numbers 
+  and multiple digit numbers like in the example above. 
+  So "55Hello" and "5Hello 5" should return two different answers. 
+  Each string will contain at least one letter or symbol. 
 */
 
-function FCN(){
-
+function NumberAddition(str){
+  return str.replace(/\D+/g, ' ').split(' ').reduce((a, b) => (+a) + (+b));
 }
 
-module.exports = FCN;
+module.exports = NumberAddition;
 
 /*
   
-
+  function NumberAddition(str) { 
+    var str = str.replace(/\D+/g, ' ').match(/\d+/g);
+    if(str !== null){
+        return str.reduce(function(acc, curr){
+            return acc += Number(curr);
+        }, 0);
+    }else{
+        return 0;
+    }         
+  }
 */
--- src/specs/25_number_addition.spec.js
+++ src/specs/25_number_addition.spec.js
@@ -1,19 +1,25 @@
 const assert = require('assert');
 const path = require('path');
-const FNAME = require(path.resolve('src/prompts', 'FCN'));
+const NumberAddition = require(path.resolve('src/prompts', '25_number_addition'));
 
 /*
+  Input:"75Number9"
+  Output:84
 
+  Input:"10 2One Number*1*"
+  Output:13
 
 */
 
-describe('FCN', ()=>{
+describe('25_number_addition', ()=>{
   it('should be a function', ()=>{
-    assert.strictEqual(typeof FNAME, 'function');
+    assert.strictEqual(typeof NumberAddition, 'function');
   });
 
-  it('should be a function', ()=>{
-    assert.strictEqual(FNAME(), );
-    assert.strictEqual(FNAME(), );
+  it('should return the sum of the string', ()=>{
+    assert.strictEqual(NumberAddition('75Number9'), 84);
+    assert.strictEqual(NumberAddition('10 2One Number*1*'), 13);
+    assert.strictEqual(NumberAddition('88Hello 3World!'), 91);
+    assert.strictEqual(NumberAddition('5Hello 5'), 10);
   });
 });
